19fae524640025974de41a5faad3abcd0e36aa6a
Foyjul Islam Raju <75216402+Foyjul-Islam-Raju@users.noreply.github.com>
2025-11-02 16:01:01 +0600

    Update 1A Theatre Square.cpp

--- 1A Theatre Square.cpp
+++ 1A Theatre Square.cpp
@@ -2,7 +2,6 @@
             "Bismillahir Rahmanir Rahim"
             "Read In the name of Allah, the most kind, the most merciful, who created us!!!"
             "Author: Foyjul Islam Raju"
-            "Dept. of CSE, City University, Bangladesh"
 -------------------------------------------------------------------------------------------*/
 
 #include <bits/stdc++.h>
@@ -30,4 +29,5 @@ int main()
       } 
     cout<<x*y<<endl;
     return 0;
+
 }
